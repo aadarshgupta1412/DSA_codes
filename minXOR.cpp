147d0423e05c22d26dbf16e9accd05c786a0db21
Aadarsh Gupta <62350692+aadarshgupta1412@users.noreply.github.com>
2022-08-21 22:54:28 +0530

    Create minXOR.cpp

---
+++ minXOR.cpp
@@ -0,0 +1,34 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int minXOR_nsquare(int arr[], int n){
+    int min = arr[0]*arr[1];
+    for (int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            int var;
+            var = arr[i] ^ arr[j];
+            if (var<min){
+                min = var;
+            }
+        }
+    }
+    return min;
+}
+
+int main(){
+    cout << "Enter the size of array: ";
+    int n;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the array elements: ";
+    for (int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int x = minXOR_nsquare(arr, n);
+    cout << "The minimum XOR is:" << x << endl;
+}
+
+
+
+

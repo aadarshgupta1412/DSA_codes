1efe2c825de53f7acb428d5e5055d32364355292
Aadarsh Gupta <62350692+aadarshgupta1412@users.noreply.github.com>
2022-09-12 22:34:38 +0530

    Create quick_sort.cpp

---
+++ quick_sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i = low-1;
+    
+    for (int j=low; j<high; j++){
+        if (arr[j]<=pivot){
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    
+    swap(arr[i+1], arr[high]);
+    return (i+1);
+}
+
+int quickSort(int arr[], int low, int high){
+    if (low<high){
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi-1);
+        quickSort(arr, pi+1, high);
+    }
+}
+
+
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the array: ";
+    for (int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"Sorted array is: ";
+    quickSort(arr, 0, n-1);
+    for (int x=0; x<n; x++){
+        if (x==n-1){
+            cout<<arr[x]<<endl;
+        }
+        else{
+            cout<<arr[x]<<' ';
+        }
+    }
+}

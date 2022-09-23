156606bbcaacce68ad676011957b23343de374a6
Aadarsh Gupta <62350692+aadarshgupta1412@users.noreply.github.com>
2022-09-24 00:01:42 +0530

    Update domino-and-tromino-tiling.cpp

--- Problems/tiling_problem/domino-and-tromino-tiling.cpp
+++ Problems/tiling_problem/domino-and-tromino-tiling.cpp
@@ -1,8 +1,10 @@
-int main(){
-    
-    int mod=1e9+7;
-    int solve(int n,vector<int>&dp){
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solve(int n, vector<int>dp){
         //base case
+        int mod=1e9+7;
         if(n==1)return 1;
         if(n==2)return 2;
         if(n==3)return 5;
@@ -11,10 +13,19 @@ int main(){
             return dp[n];
         return 
             dp[n]=((2*solve(n-1,dp))%mod + solve(n-3,dp)%mod)%mod;
-    }
-    int numTilings(int n) {
-        
+}
+
+int numTilings(int n) {
         vector<int>dp(n+10,-1);
         return solve(n,dp);
+}
+
+int main(){
+
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int x=numTilings(n);
+    cout<<"Number of ways of tiling: "<<x;
 
 }

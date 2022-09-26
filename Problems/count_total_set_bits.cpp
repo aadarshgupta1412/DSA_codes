7feda1fdb1338a47135a4214893a2ec382c5ab80
Aadarsh Gupta <62350692+aadarshgupta1412@users.noreply.github.com>
2022-09-26 19:20:04 +0530

    Update count_total_set_bits.cpp

--- Problems/count_total_set_bits.cpp
+++ Problems/count_total_set_bits.cpp
@@ -1,5 +1,9 @@
 // Problem: https://www.interviewbit.com/problems/count-total-set-bits/
 
+// 2 methods:
+// 1) start from msb and for every 1s, count number of smaller no.s that can be formed, and add 2^n*encountered_1s_so_far
+// 2) last bit flips every 2nd number, second last flips every 4th number and so on.. count them till the number
+
 int Solution::solve(int A) {
     
     if (A==1){
@@ -24,3 +28,23 @@ int Solution::solve(int A) {
     }
     return count%modi;
 }
+
+// Method 2: InterviewBit
+// int Solution::solve(int A) {
+//    long long  int ans=0;
+   
+//    int bits=log10(A)/log10(2);
+//    //cout<<bits;
+//    for(int i=0;i<=bits;i++)
+//    {
+//        int c=pow(2,i),cnt=0;
+//        int set=(A+1)/c;
+//        cnt=(set/2)*c;
+//        int incomp=(A+1)%c;
+//        if(set%2!=0) cnt+=incomp;
+       
+//        ans+=cnt;
+//    }
+//     return ans%1000000007;
+// }
+

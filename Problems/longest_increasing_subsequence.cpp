84cb26d8b667de70ca404525e87869f519bf2763
Aadarsh Gupta <62350692+aadarshgupta1412@users.noreply.github.com>
2022-10-21 19:33:28 +0530

    Update longest_increasing_subsequence.cpp

--- Problems/longest_increasing_subsequence.cpp
+++ Problems/longest_increasing_subsequence.cpp
@@ -2,7 +2,35 @@
 // https://www.interviewbit.com/blog/longest-increasing-subsequence/
 // https://www.geeksforgeeks.org/longest-increasing-subsequence-dp-3/
 
-class Solution {
+// Time Complexity: O(n^2)
+class Solution1 {
+public:
+    
+    int lis(vector<int> nums){
+        int n = nums.size();
+        vector<int> lis(n);
+        lis[0] = 1;
+        for (int i=1; i<n; i++){
+            lis[i] = 1;
+            for (int j=0; j<i; j++){
+                if (nums[i]>nums[j] && lis[i]<lis[j]+1){
+                    lis[i]=lis[j]+1;
+                }
+            } 
+        }
+        return *max_element(lis.begin(), lis.end());
+    }
+    
+    int lengthOfLIS(vector<int> nums){
+        int x = lis(nums);
+        return x;
+    }
+    
+};
+
+
+// Time Complexity: O(n*log(n))
+class Solution2 {
 public:
     
     int find_idx(vector<int> a, int entry){

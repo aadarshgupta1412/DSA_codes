0b99c9ce3564aab08625689b7aa487b1c2b0edd0
Aadarsh Gupta <62350692+aadarshgupta1412@users.noreply.github.com>
2022-11-19 17:31:48 +0530

    Update 0_1_knapsack_problem.cpp

--- Problems/0_1_knapsack_problem.cpp
+++ Problems/0_1_knapsack_problem.cpp
@@ -1,34 +1,28 @@
-// Problem : https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/ticky_on_article
+// Problem: https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/ticky_on_article
+// Solution resources: 
+// https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+// https://www.interviewbit.com/blog/0-1-knapsack-problem/
 
-class Solution
-{
+
+class Solution{
     public:
+    
+    int knap(int N, int W, int w[], int v[], vector<vector<int>> &dp){
+        if (N<0) return 0;
+        if (dp[N][W]!=-1) return dp[N][W];
+        if (w[N]<=W){
+            dp[N][W] = max(v[N]+knap(N-1, W-w[N], w, v, dp), knap(N-1, W, w, v, dp));
+            return dp[N][W];
+        }
+        else{
+            dp[N][W] = knap(N-1, W, w, v, dp);
+            return dp[N][W];
+        }
+    }
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-       int i, w; 
-       int K[n+1][W+1]; 
-      
-       //building table K[][] in bottom up manner.
-       for (i = 0; i <= n; i++) 
-       { 
-           for (w = 0; w <= W; w++) 
-           { 
-                //base case  
-                if (i==0 || w==0) 
-                    K[i][w] = 0;
-                
-                //if weight of current item is more than Knapsack capacity W
-                //then this item cannot be included in the optimal solution. 
-                else if (wt[i-1] <= w) 
-                    K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]); 
-                    
-                //else updating K[i][w] as K[i-1][w].   
-                else
-                    K[i][w] = K[i-1][w]; 
-           } 
-       } 
-       //returning the result.
-       return K[n][W]; 
+       vector<vector<int>> dp(n+1, vector<int>(W+1, -1));
+       return knap(n-1, W, wt, val, dp);
     }
 };
